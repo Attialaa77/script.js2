525ca1708079924b10742c063d0cd48846d35027
Attialaa77 <n01608060@humber.ca>
2024-09-20 20:38:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("hello class")
+
+var studentname= "Alaa"; // declaring student's name
+var studentage=21;
+var isEnrolled = true; 
+
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Is Enrolled:", isEnrolled);
+
+// Array: A collection of values
+let arrayExample = {mango,cherry,peach};
+//console.log(studentname);
+
+let studentProgram = "CPAN"; // declaring the student's program
+studentProgram = "CP"; // Updating the student's program
+// let studentProgram = "Web Design"; // Test and see what happens?
+
+console.log(studentProgram)
